29324513042fa90344b159f657d75116e85f3fc9
wangchen <wangchen19941211@126.com>
2022-10-22 15:04:01 +0800

    feat:导入添加事务

--- acspace-service/acspace-service-subject/src/main/java/com/acspace/service/impl/SubjectServiceImpl.java
+++ acspace-service/acspace-service-subject/src/main/java/com/acspace/service/impl/SubjectServiceImpl.java
@@ -14,6 +14,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.util.StringUtils;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -61,6 +62,7 @@ public class SubjectServiceImpl extends ServiceImpl<SubjectMapper, Subject> impl
      * 导入题目
      * @param file
      */
+    @Transactional
     @Override
     public void importSubject(MultipartFile file) {
         try {
